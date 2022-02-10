dd13030f6eaa7013167625c32a70163270f62c46
viideguzman31 <viideguzman@yahoo.com>
2022-02-11 00:31:14 +0800

    fuckin typo in worker-deployment

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib Calculator - Updated V3
+            Fib Calculator - Updated V4
           </a>
         <Link to="/">Home</Link>
         <Link to="/otherPage">Other Page</Link>
