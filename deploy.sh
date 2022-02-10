f902c20f14a5acdc48d05f0d2cdea42b681c4b5b
viideguzman31 <viideguzman@yahoo.com>
2022-02-10 23:50:10 +0800

    modify travis due to next line -t issue

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,6 @@
-docker build 
--t viideguzman/multi-client:latest 
--t viideguzman/multi-client:$GIT_SHA
--f ./client/Dockerfile ./client
-docker build 
--t viideguzman/multi-server:latest 
--t viideguzman/multi-server:$GIT_SHA
--f ./server/Dockerfile ./server
-docker build 
--t viideguzman/multi-worker:latest  
--t viideguzman/multi-worker:$GIT_SHA 
--f ./worker/Dockerfile ./worker
+docker build -t viideguzman/multi-client:latest -t viideguzman/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t viideguzman/multi-server:latest -t viideguzman/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t viideguzman/multi-worker:latest  -t viideguzman/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
 
 docker push viideguzman/multi-client:latest 
 docker push viideguzman/multi-server:latest 
